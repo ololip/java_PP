6cd924b1182a4fe97a29a6bec0fde3deb110f983
ololip <oliwerlipinski96@gmail.com>
2023-03-25 09:51:27 +0100

    first commit

---
+++ src/main/java/com/sda/Main.java
@@ -0,0 +1,9 @@
+package com.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        String Asd = "asd";
+        System.out.println(Asd);
+        if(false) System.out.println("zxc");
+    }
+}
